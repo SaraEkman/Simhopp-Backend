52618b70fe7411e36ab86ce564b1d51c71477e0d
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-01-06 15:53:32 +0100

    softDelete a user works + test

--- routes/users.js
+++ routes/users.js
@@ -34,11 +34,8 @@ router.post('/', (req, res, next) => {
     else if (req.body.userEmail == null || req.body.userEmail == undefined || req.body.userEmail == '') {
       return res.json({ message: 'Email is required!' });
     }
-    else if (req.body.admin == null || req.body.admin == undefined || req.body.admin == ''
-      || req.body.admin == 'undefined' || req.body.admin == 'null') {
-      req.body.admin = 0;
-
-      let sql = `INSERT INTO users (userName, userEmail, admin, password) VALUES ('${req.body.userName}', '${req.body.userEmail}', '${req.body.admin}', '${req.body.password}')`;
+    else {
+      let sql = `INSERT INTO users (userName, userEmail, password) VALUES ('${req.body.userName}', '${req.body.userEmail}', '${req.body.password}')`;
 
       req.app.locals.con.query(sql,
         (err, result) => {
@@ -58,17 +55,20 @@ router.put('/:id', (req, res, next) => {
     if (req.body.userName == null || req.body.userName == undefined || req.body.userName == '') {
       return res.json({ message: 'Username is required!' });
     }
-    else if (req.body.password == null || req.body.password == undefined || req.body.password == '') {
-      return res.json({ message: 'Password is required!' });
-    }
     else if (req.body.userEmail == null || req.body.userEmail == undefined || req.body.userEmail == '') {
       return res.json({ message: 'Email is required!' });
     }
-    else if (req.body.admin == null || req.body.admin == undefined || req.body.admin == ''
-      || req.body.admin == 'undefined' || req.body.admin == 'null') {
-      req.body.admin = 0;
-      let sql = `UPDATE users SET userName = '${req.body.userName}', userEmail = '${req.body.userEmail}', password = '${req.body.password}' , admin = '${req.body.admin}' WHERE id = '${req.params.id}'`;
+    else if (req.body.password == null || req.body.password == undefined || req.body.password == '') {
+      return res.json({ message: 'Password is required!' });
+    }
 
+    else {
+      let sql;
+      if (req.body.admin == 1 || req.body.admin == 0 && req.body.softDelete == 1 || req.body.softDelete == 0) {
+        sql = `UPDATE users SET userName = '${req.body.userName}', userEmail = '${req.body.userEmail}', password = '${req.body.password}', admin = '${req.body.admin}', softDelete = '${req.body.softDelete}' WHERE id = '${req.params.id}'`;
+      } else {
+        sql = `UPDATE users SET userName = '${req.body.userName}', userEmail = '${req.body.userEmail}', password = '${req.body.password}' WHERE id = '${req.params.id}'`;
+      }
       req.app.locals.con.query(sql,
         (err, result) => {
           if (err) throw err;
@@ -80,26 +80,6 @@ router.put('/:id', (req, res, next) => {
     }
   });
 });
-
-
-// ! This is not working, because it is forgin key in news is connected there, but it should be possible to delete a user
-// router.delete('/:id', (req, res, next) => {
-//   req.app.locals.con.connect((err) => {
-//     if (err) {
-//       console.log('Error connecting to Db');
-//     }
-//     let sql = `DELETE FROM users WHERE id = '${req.params.id}'`;
-
-//     req.app.locals.con.query(sql,
-//       (err, result) => {
-//         if (err) throw err;
-//         console.log("1 record inserted", result);
-//       });
-//     res.json({
-//       message: 'User deleted!'
-//     });
-//   });
-// });
 
 
 module.exports = router;
