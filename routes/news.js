4c2253f01ca4d5902c217a3ff2b9695523e7f1a8
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-01-06 14:30:53 +0100

    edit news works + test

--- routes/news.js
+++ routes/news.js
@@ -22,15 +22,48 @@ router.post('/', (req, res, next) => {
         if (err) {
             console.log('Error connecting to Db');
         }
-        let sql = `INSERT INTO news (content, userId) VALUES ('${req.body.content}', '${req.body.userId}')`;
-        req.app.locals.con.query(sql,
-            (err, result) => {
-                if (err) throw err;
-                console.log("1 record inserted", result);
-            });
+        if (req.body.content == null || req.body.content == undefined || req.body.content == '') {
+            return res.json({ message: 'Content is required!' });
+        } else if (req.body.userId == null || req.body.userId == undefined || req.body.userId == '') {
+            return res.json({ message: 'User is required!' });
+        } else if (req.body.userId == null || req.body.userId == undefined || req.body.userId == ''
+            || req.body.userId == 'undefined' || req.body.userId == 'null') {
+            res.json({ message: 'User is required!' });
+        } else {
+            let sql = `INSERT INTO news (content, userId) VALUES ('${req.body.content}', '${req.body.userId}')`;
+            req.app.locals.con.query(sql,
+                (err, result) => {
+                    if (err) throw err;
+                    console.log("1 record inserted", result);
+                });
+
+            res.json({ message: 'News created!' });
+        }
     });
-    res.json({ message: 'News created!' });
 });
+
+router.put('/:id', (req, res, next) => {
+    req.app.locals.con.connect((err) => {
+        if (err) {
+            console.log('Error connecting to Db');
+        }
+        if (req.body.content == null || req.body.content == undefined || req.body.content == '') {
+            return res.json({ message: 'Content is required!' });
+        } else if (req.body.userId == null || req.body.userId == undefined || req.body.userId == '') {
+            return res.json({ message: 'UserId is required!' });
+        } else {
+            let sql = `UPDATE news SET content = '${req.body.content}', userId = '${req.body.userId}' WHERE id = '${req.params.id}'`;
+            req.app.locals.con.query(sql,
+                (err, result) => {
+                    if (err) throw err;
+                    console.log("1 record inserted", result);
+                });
+            
+            res.json({ message: 'News updated!' });
+        }
+    });
+});
+
 
 
 
