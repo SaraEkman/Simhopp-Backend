0a4751c4cac513990e6d5242b7c8ed4061f293f4
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-02-05 21:03:33 +0100

    functionality to upload images to news & get and delete images

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql2');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,8 +15,25 @@ var app = express();
 app.use(cors());
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 100000, limit: '50mb' }));
+
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) =>{
+        callBack(null, '../Simhopp/src/assets/uploads');
+    },
+    filename: (req, file, callBack) =>{
+        callBack(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
+// app.use(multer({ limits: { fileSize: 50 * 1024 * 1024 } }).single('image'));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
@@ -37,5 +56,14 @@ app.locals.con.connect((err) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
+
+
+app.post('/uploads', upload.single('image'), (req, res, next) => {
+    console.log(req.body);
+    if (req.file == null) {
+        return res.status(500).json({ message: 'No image selected' });
+    }
+    res.send(req.file);
+});
 
 module.exports = app;
--- routes/news.js
+++ routes/news.js
@@ -4,19 +4,21 @@ var checkAdmin = require('../services/checkAdmin');
 var router = express.Router();
 
 router.post('/add', auth.authenticateToken, checkAdmin.checkAdmin, (req, res, next) => {
-    sql = "insert into news (content, userId) values(?,?)";
-    req.app.locals.con.query(sql, [req.body.content, req.body.userId], (err, result) => {
+    sql = "insert into news (content, userId, image) values(?,?,?)";
+    req.app.locals.con.query(sql, [req.body.content, req.body.userId, req.body.image], (err, result) => {
+        console.log(result);
         if (!err) {
-            return res.status(200).json({ message: 'News added successfully' });
+            return res.status(200).json({ message: 'News added successfully', id: result.insertId });
         } else {
             return res.status(500).json(err);
         }
     });
 });
 
+
 router.get('/', (req, res, next) => {
     console.log(req.query.limit);
-    sql = "select id,content,createDate from news where softDelete = 0 order by createDate desc limit " + req.query.limit;
+    sql = "select id,content,createDate,image from news where softDelete = 0 order by createDate desc limit " + req.query.limit;
     console.log(sql);
     req.app.locals.con.query(sql, (err, result) => {
         console.log(result);
@@ -31,7 +33,7 @@ router.get('/', (req, res, next) => {
 
 
 router.get('/get', auth.authenticateToken, (req, res, next) => {
-    sql = "select news.id,news.content,news.createDate,news.userId,news.softDelete,users.userName from news INNER JOIN users on news.userId = users.id order by createDate desc";
+    sql = "select news.id,news.content,news.image,news.createDate,news.userId,news.softDelete,users.userName from news INNER JOIN users on news.userId = users.id order by createDate desc";
     req.app.locals.con.query(sql, (err
         , result) => {
         console.log(result);
@@ -44,8 +46,8 @@ router.get('/get', auth.authenticateToken, (req, res, next) => {
 });
 
 router.patch('/update', auth.authenticateToken, checkAdmin.checkAdmin, (req, res, next) => {
-    sql = "update news set content = ?, userId = ? where id = ?";
-    req.app.locals.con.query(sql, [req.body.content, req.body.userId, req.body.id], (err, result) => {
+    sql = "update news set content = ?, image = ?, userId = ? where id = ?";
+    req.app.locals.con.query(sql, [req.body.content, req.body.image, req.body.userId, req.body.id], (err, result) => {
         if (!err) {
             if (result.affectedRows == 0) {
                 return res.status(404).json({ message: 'News id not found' });
@@ -71,19 +73,4 @@ router.patch('/delete', auth.authenticateToken, checkAdmin.checkAdmin, (req, res
     }); 
 });
         
-// router.delete('/delete/id', auth.authenticateToken, checkAdmin.checkAdmin, (req, res, next) => {
-//     sql = "update news set softDelete = ? where id = ?";
-//     req.app.locals.con.query(sql, [req.body.softDelete, req.body.id], (err, result) => {
-//         if (!err) {
-//             if (result.affectedRows == 0) {
-//                 return res.status(404).json({ message: 'News id not found' });
-//             }
-//             return res.status(200).json({ message: 'News deleted successfully' });
-//         } else {
-//             return res.status(500).json(err);
-//         }
-//     });
-// });
-
-
 module.exports = router;
