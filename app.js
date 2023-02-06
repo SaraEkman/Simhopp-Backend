7799545196826cc1e914d0299395ae96faa9320a
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-02-06 16:19:19 +0100

    add port

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ require('dotenv').config();
 app.locals.con = mysql.createConnection({
     connectionLimit: 10,
     host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
