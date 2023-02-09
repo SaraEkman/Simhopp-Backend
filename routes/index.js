b4a93e51fb93599aaa222c20d1b5c0b281bbd859
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-02-09 16:59:05 +0100

    add cors

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  sql = "select * from users";
-  req.app.locals.con.query(sql, (err, result) => {
-    console.log(result);
-    if (!err) {
-      // return res.status(200).json(result);
-      res.json(result);
-    } else {
-      return res.status(500).json(err);
-    }
-  });
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
