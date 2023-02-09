cac4b19d579e601ba0b62772cd0bc1408be21116
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-02-09 16:22:11 +0100

    try new deploy

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  sql = "select * from users";
+  req.app.locals.con.query(sql, (err, result) => {
+    console.log(result);
+    if (!err) {
+      // return res.status(200).json(result);
+      res.render('index', result);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+
 });
 
 module.exports = router;
