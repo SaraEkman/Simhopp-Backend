568c371c0191c0123db527e6c90fbb7f11e16d53
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-02-09 01:39:47 +0100

    add limit

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
 
 app.locals.con = mysql.createConnection({
-    // connectionLimit: 10,
+    connectionLimit: 10,
     // multipleStatements: true,
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT ,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
